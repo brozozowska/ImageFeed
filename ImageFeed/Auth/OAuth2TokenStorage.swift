b508185e82458a4bc04cdf5f40f49b354ccf6e2c
brozozowska <s_rozov@me.com>
2025-07-01 18:16:14 +0300

    feat(auth): хранение токена перенесено из UserDefaults в Keychain добавлен SwiftKeychainWrapper в проект

--- ImageFeed/Auth/OAuth2TokenStorage.swift
+++ ImageFeed/Auth/OAuth2TokenStorage.swift
@@ -6,20 +6,26 @@
 //
 
 import Foundation
+import SwiftKeychainWrapper
 
 final class OAuth2TokenStorage {
+    
     private enum Keys: String {
         case token
     }
     
-    private let userDefaults = UserDefaults.standard
+    private let keychain = KeychainWrapper.standard
     
     var token: String? {
         get {
-            userDefaults.string(forKey: Keys.token.rawValue)
+            keychain.string(forKey: Keys.token.rawValue)
         }
         set {
-            userDefaults.set(newValue, forKey: Keys.token.rawValue)
+            if let newValue {
+                keychain.set(newValue, forKey: Keys.token.rawValue)
+            } else {
+                keychain.removeObject(forKey: Keys.token.rawValue)
+            }
         }
     }
 }
